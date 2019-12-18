6bdfff171426e1b009e9babcab30b944e01ce948
lyn <lyn@lyn.person>
2019-12-19 00:08:07 +0800

    Constants with all UPPER CASE

---
+++ src/main/java/chapter3_fundamental/Constant_3_4.java
@@ -0,0 +1,10 @@
+package chapter3_fundamental;
+
+public class Constant_3_4 {
+    public static void main(String[] args) {
+        final double PI = 3.1415926; // constant using all UPPER CASE
+
+        System.out.println(PI * 3 * 3);
+
+    }
+}
