0a7df9643853c0a053317cdf38b2b01c59b54530
lyn <lyn@lyn.person>
2019-12-18 23:58:15 +0800

    one line comment & multiple line Comment_3_2

---
+++ src/main/java/chapter3_fundamental/Comment_3_2.java
@@ -0,0 +1,15 @@
+package chapter3_fundamental;
+
+public class Comment_3_2 {
+    public static void main(String[] args) {
+        System.out.println("care the comment");
+//        System.out.println("one line comment");
+        /*
+        System.out.println("multiple");
+        System.out.println("lines");
+        System.out.println("comment");
+
+         */
+
+    }
+}
--- src/main/java/chapter3_fundamental/Simple_3_1.java
+++ src/main/java/chapter3_fundamental/Simple_3_1.java
@@ -1,6 +1,6 @@
 package chapter3_fundamental;
 
-public class Simple {
+public class Simple_3_1 {
     public static void main(String[] args) {
         System.out.println("first program, not using hello world");
     }
