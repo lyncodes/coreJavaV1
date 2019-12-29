7192c6fc5979eb764e0275136fe10453def7b538
lyn <lyn@lyn.person>
2019-12-29 16:12:32 +0800

    how to thrown an Exception 7_1_3

---
+++ src/main/java/chapter7_Exception_Assert_Logging/Method_thrown_exception_7_1_3.java
@@ -0,0 +1,22 @@
+package chapter7_Exception_Assert_Logging;
+
+/**
+ * author lyn
+ * create_date_time 2019/12/28 0:01
+ */
+public class Method_thrown_exception_7_1_3 {
+    public static void main(String[] args) {
+        Method_thrown_exception_7_1_3 m = new Method_thrown_exception_7_1_3();
+        m.throwException(5);
+        m.throwException(10);
+    }
+
+    public void throwException(Integer i) {
+        if (i < 10) {
+            System.out.println("no exception");
+        } else {
+            String reason = "the Exception number input is " + i;
+            throw new ArithmeticException(reason);
+        }
+    }
+}
