2610bf32e5f61ca08375a3906c1f2789111c2cfe
lyn <lyn@lyn.person>
2019-12-31 23:28:15 +0800

    finally clause

---
+++ src/main/java/chapter7_Exception_Assert_Logging/Finally_7__2_4.java
@@ -0,0 +1,21 @@
+package chapter7_Exception_Assert_Logging;
+
+/**
+ * author lyn
+ * create_date_time 2019/12/31 23:12
+ */
+public class Finally_7__2_4 {
+    public static void main(String[] args) {
+        System.out.println("finally clause");
+
+        try{
+            System.out.println("open resource");
+            // business code
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("close resource");
+        }
+
+    }
+}
