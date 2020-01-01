2982d713a9549856448b3b9f08990730cb5952a0
lyn <lyn@lyn.person>
2020-01-01 23:58:59 +0800

    basic generic class with single T para or T,U multiple type paras

---
+++ src/main/java/chapter8_Generic_Programming/DefiningGenericClass_8_2.java
@@ -0,0 +1,52 @@
+package chapter8_Generic_Programming;
+
+import lombok.*;
+import sun.dc.pr.PRError;
+
+/**
+ * author lyn
+ * create_date_time 2020/1/1 23:20
+ */
+
+
+public class DefiningGenericClass_8_2 {
+    public static void main(String[] args) {
+        MyGeneric<Integer> dg = new MyGeneric<>();
+        dg.setX(12);
+        dg.setY(13);
+        System.out.println(dg.toString());
+
+        MyGeneric<Double> dg2 = new MyGeneric<>();
+        dg2.setX(12.0);
+        dg2.setY(13.0);
+        System.out.println(dg2.toString());
+
+
+        SeparateGeneric<String, Integer> sg = new SeparateGeneric<>("12",12);
+        System.out.println(sg.toString());
+    }
+
+
+
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+class MyGeneric<T>{
+    private T x;
+    private T y;
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+class SeparateGeneric<T,U>{
+    // using T U to specify multiple type of parameter
+    private T x;
+    private U y;
+}
