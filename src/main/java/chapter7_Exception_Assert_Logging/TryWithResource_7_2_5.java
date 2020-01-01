57711d50fe97a806d65d46aa55882232496e7aa3
lyn <lyn@lyn.person>
2020-01-01 13:10:37 +0800

    try with resource syntax that the resource must implement the Closable interface

---
+++ src/main/java/chapter7_Exception_Assert_Logging/TryWithResource_7_2_5.java
@@ -0,0 +1,42 @@
+package chapter7_Exception_Assert_Logging;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * author lyn
+ * create_date_time 2020/1/1 12:42
+ */
+public class TryWithResource_7_2_5 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("TryWithResource_7_2_5");
+
+        try (MyConnection mc = new MyConnection()) {
+            mc.send(15);
+            mc.send(9);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class MyConnection implements Closeable {
+
+    // an Object that need to be autoRecycle the resource
+    // must implement the Closable interface.`
+    @Override
+    public void close() {
+        System.out.println("Now release the resource.");
+    }
+
+    public void send(Integer i) throws Exception {
+        System.out.println("sent data");
+        if (i > 10) {
+            System.out.println("fine");
+        } else {
+            throw new Exception("new Exception");
+        }
+    }
+}
