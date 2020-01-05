da8b085bd3fcd03e451ec0757439fd09d5543da0
lyn <lynmeig@live.com>
2020-01-05 22:04:34 +0800

    ArrayList_9_2_2

---
+++ src/main/java/chapter9_Collections/JavaCollectionFramework_9_1/ConcreteCollections_9_2/ArrayList_9_2_2.java
@@ -0,0 +1,12 @@
+package chapter9_Collections.JavaCollectionFramework_9_1.ConcreteCollections_9_2;
+
+/**
+ * author lyn
+ * create_date_time 2020/1/5 22:01
+ */
+public class ArrayList_9_2_2 {
+    public static void main(String[] args) {
+        System.out.println("similar with the linkedList, and a" +
+                "dynamic reallocated array of objects");
+    }
+}
