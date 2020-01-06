5ce03928e7461b8999647e5649730df43bed7d11
lyn <lynmeig@live.com>
2020-01-06 20:17:53 +0800

    HashSets_9_2_3

---
+++ src/main/java/chapter9_Collections/JavaCollectionFramework_9_1/ConcreteCollections_9_2/HashSets_9_2_3.java
@@ -0,0 +1,18 @@
+package chapter9_Collections.JavaCollectionFramework_9_1.ConcreteCollections_9_2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * author lyn
+ * create_date_time 2020/1/5 22:19
+ */
+public class HashSets_9_2_3 {
+    public static void main(String[] args) {
+        HashSet<String> ss = new HashSet<>();
+        ss.add("1");
+        ss.add("1");
+        ss.add("2");
+        System.out.println(ss);
+    }
+}
