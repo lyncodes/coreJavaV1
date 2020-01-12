71b20fa538d494c5863eab9afe36ed6edfb6c10a
lyn <lynmeig@live.com>
2020-01-12 23:48:40 +0800

    add ReentrantLock to avoid thread-unsafe condition

--- src/main/java/chapter14_Concurrency/RaceCondition_14_5_1/Bank.java
+++ src/main/java/chapter14_Concurrency/RaceCondition_14_5_1/Bank.java
@@ -1,6 +1,8 @@
 package chapter14_Concurrency.RaceCondition_14_5_1;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * author lyn
@@ -9,18 +11,25 @@ import java.util.Arrays;
 public class Bank {
     private final double[] accounts;
 
+    private Lock bankLock = new ReentrantLock();
+
     public Bank(int n, double initialBalance) {
         this.accounts = new double[n];
         Arrays.fill(accounts, initialBalance);
     }
 
     public void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) return;
-        System.out.println(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf("%10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf("Total Balance: %10.2f%n", getTotalBalnce());
+        bankLock.lock();
+        try {
+            if (accounts[from] < amount) return;
+            System.out.println(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf("%10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf("Total Balance: %10.2f%n", getTotalBalnce());
+        } finally {
+            bankLock.unlock();
+        }
     }
 
     public double getTotalBalnce() {
